5b3246439f673770f189aea4c9745a262da2046e
PhamDat328 <92577783+PhamDat328@users.noreply.github.com>
2022-12-02 14:21:07 +0700

    push to github

---
+++ product/product.js
@@ -0,0 +1,12 @@
+let main_picture = document.querySelector(".main-picture img")
+let picture_Menu = document.querySelectorAll(".others-picture img")
+
+picture_Menu.forEach(function(picture) {
+
+    picture.addEventListener("click", function() {
+
+        main_picture.src = picture.src
+
+    })
+
+})
